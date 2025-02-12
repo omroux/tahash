caef579f68925b14d01ae3595b1567bd99934dc3
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2025-02-12 22:04:14 +0200

    Request specific files from src directory + home page (with extra fonts!!11!!1)

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -6,9 +8,17 @@ const PORT = 3000;
 // home page
 app.get("/", (req, res) => {
     console.log(`Client with ip "${req.ip}" connected`);
-    res.send("<h1>tahash moment</h1>");
+    // res.send("<h1>tahash moment</h1>");
+    res.redirect("src/pages/home.html");
 });
 
+// get a source file
+app.get("/src/*", (req, res) => {
+    const filePath = path.join(__dirname, req.url);
+    if (!fs.existsSync(filePath))
+        res.status(404).send(`File ${req.url} not found.`);
+    res.sendFile(filePath);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
