f0ac1f82c9272716a15a20c75704e6aff1349115
Idan Sahar <36777029+IdanCodes@users.noreply.github.com>
2025-05-31 15:40:57 +0300

    added event box handling to admin dashboard

--- backend/src/scripts/pages/admin-dashboard.js
+++ backend/src/scripts/pages/admin-dashboard.js
@@ -1,17 +1,40 @@
 import { resizeEventBoxes } from "/src/scripts/backend/utils/eventBoxesUtils.js";
 
+const eventSelectContainer = document.getElementById("eventSelectContainer");
+const eventBoxes = [];
+
 onPageLoad(async () => {
     const isAdmin = await getAdminPerms();
     if (!isAdmin) {
         window.location = "/";
         return;
     }
-
-
-
+    
+    addEventBox("333", "3x3x3", "event-333");
+    addEventBox("333", "3x3x3", "event-333");
+    addEventBox("333", "3x3x3", "event-333");
+    addEventBox("333", "3x3x3", "event-333");
     setLoadingState(false);
 });
 
-function addEventBox(eventImg) {
+function addEventBox(eventId, eventTitle, eventIconName) {
+    // event box
+    const eventBoxEl = document.createElement("div");
+    eventBoxEl.id = `event-select-${eventId}`;
+    eventBoxEl.className = "Event-Select-Box";
+
+    // event icon
+    const eventIconEl = document.createElement("span");
+    eventIconEl.className = `Cubing-Icon ${eventIconName}`;
+    eventBoxEl.appendChild(eventIconEl);
     
+    // event title
+    const eventTitleEl = document.createElement("p");
+    eventTitleEl.className = "Event-Name-Title";
+    eventTitleEl.innerText = eventTitle;
+    eventBoxEl.appendChild(eventTitleEl);
+
+    eventBoxes.push(eventBoxEl);
+    resizeEventBoxes([eventBoxEl]);
+    eventSelectContainer.appendChild(eventBoxEl);
 }
